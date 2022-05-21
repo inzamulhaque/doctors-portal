f7632b87d0487cbc70a02cbf001ba3a2c3c39612
inzamulhaque <mdihalif@gmail.com>
2022-05-21 19:08:46 +0600

    fix 404 problem

--- index.js
+++ index.js
@@ -284,9 +284,17 @@ async function run() {
 
 run().catch(console.dir);
 
-if (process.env.NODE_ENV == "production") {
-    app.use(express.static("client/build"));
-}
+// if (process.env.NODE_ENV == "production") {
+//     app.use(express.static("client/build"));
+// }
+
+app.get('/*', function (req, res) {
+    res.sendFile(path.join(__dirname, 'client/build/index.html'), function (err) {
+        if (err) {
+            res.status(500).send(err)
+        }
+    })
+})
 
 app.listen(port, () => {
     console.log(`http://localhost:${port}`);
